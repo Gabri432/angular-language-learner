8c04fb1c1d0f630be6bb702af2536263ea28652a
Gabri432 <gabrielegatti432@gmail.com>
2023-12-31 15:50:00 +0100

    Creating basic components of the website.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { FlashcardComponent } from './layouts/flashcard/flashcard.component';
+import { HomeComponent } from './routes/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    FlashcardComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
