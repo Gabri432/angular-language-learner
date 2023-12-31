04c5d827475879662f6f2a071e88d1aac6ff2db1
Gabri432 <gabrielegatti432@gmail.com>
2023-12-31 17:29:27 +0100

    Removing no longer needed imports.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FlashcardComponent } from './layouts/flashcard/flashcard.component';
-import { HomeComponent } from './routes/home/home.component';
 import { LayoutsModule } from './layouts/layouts.module';
 import { RoutesModule } from './routes/routes.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FlashcardComponent,
-    HomeComponent
   ],
   imports: [
     BrowserModule,
