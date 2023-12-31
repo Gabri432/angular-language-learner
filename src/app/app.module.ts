f36988d1e7706c5be3b10db37dced6965bf33690
Gabri432 <gabrielegatti432@gmail.com>
2023-12-31 16:04:47 +0100

    Importing the layouts module, creating custom styles.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './layouts/header/header.component';
-import { FooterComponent } from './layouts/footer/footer.component';
 import { FlashcardComponent } from './layouts/flashcard/flashcard.component';
 import { HomeComponent } from './routes/home/home.component';
+import { LayoutsModule } from './layouts/layouts.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent,
     FlashcardComponent,
     HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LayoutsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
