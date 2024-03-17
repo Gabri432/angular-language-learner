ab6df6dd6c87c3f28cf087e15debe15f797af20a
Gabri432 <gabrielegatti432@gmail.com>
2024-03-17 19:22:15 +0100

    Generating and showing two different options.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutsModule } from './layouts/layouts.module';
 import { RoutesModule } from './routes/routes.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { RoutesModule } from './routes/routes.module';
     BrowserModule,
     AppRoutingModule,
     LayoutsModule,
-    RoutesModule
+    RoutesModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/routes/home/home.component.ts
+++ src/app/routes/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input } from '@angular/core';
+import { Word } from 'src/app/models/word.model';
+import { VocabularyService } from 'src/app/services/getWords.service';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +8,45 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  private words: Word[] = [];
+  currentOptions: Word[] = [{"english": "", "french": ""}, {"english": "", "french": ""}];
 
   @Input() frenchWord: string = "pomme";
+
+  constructor(private getWords: VocabularyService) { }
+
+  ngOnInit(): void {
+    this.getWords.getVocabulary().subscribe(data => {
+      for (const d of data) {
+        this.words.push(d);
+      }
+      this.pickNewWords();
+      this.generateWordToGuess();
+    });
+  }
+
+  selectRandomWord(): Word {
+    const pick = Math.floor(Math.random() * this.words.length);
+    return this.words[pick];
+  }
+
+  pickNewWords(): void {
+    this.currentOptions[0] = this.selectRandomWord();
+    const secondOp = this.selectRandomWord();
+    if (this.currentOptions[0].english != secondOp.english) {
+      this.currentOptions[1] = secondOp;
+    } else {
+      this.pickNewWords();
+    }
+  }
+
+  generateWordToGuess(): void {
+    const pick = Math.floor(Math.random() * this.currentOptions.length);
+    this.frenchWord = this.currentOptions[pick].french;
+  }
+
+  newTry(): void {
+    this.pickNewWords();
+    this.generateWordToGuess();
+  }
 }
